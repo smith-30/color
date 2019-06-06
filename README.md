fa9b2e418b16d56f60a9ecba39aa9039b40fbc9a
misu <misu@aptpod.co.jp>
2019-06-06 20:35:19 +0900

    mod: readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![Coverage Status](https://coveralls.io/repos/github/smith-30/color/badge.svg?branch=master)](https://coveralls.io/github/smith-30/color?branch=master)
 [![GoDoc](https://godoc.org/github.com/smith-30/color?status.svg)](https://godoc.org/github.com/smith-30/color)
 [![license](https://img.shields.io/badge/license-MIT-4183c4.svg)](https://github.com/smith-30/color/blob/master/LICENSE)
-[![GoReportCard](https://goreportcard.com/report/github.com/smith-30/color)](https://goreportcard.com/report/github.com/smith-30/color)
+[![Go Report Card](https://goreportcard.com/badge/github.com/smith-30/color)](https://goreportcard.com/report/github.com/smith-30/color)
--- main_test.go
+++ main_test.go
@@ -53,3 +53,57 @@ func TestGreen(t *testing.T) {
 		})
 	}
 }
+
+func TestRedf(t *testing.T) {
+	type args struct {
+		s    string
+		args []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{
+				s:    "s %v",
+				args: []interface{}{1},
+			},
+			want: RED + "s 1" + RESET,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Redf(tt.args.s, tt.args.args...); got != tt.want {
+				t.Errorf("Redf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGreenf(t *testing.T) {
+	type args struct {
+		s    string
+		args []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{
+				s:    "s %v",
+				args: []interface{}{1},
+			},
+			want: GREEN + "s 1" + RESET,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Greenf(tt.args.s, tt.args.args...); got != tt.want {
+				t.Errorf("Greenf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
